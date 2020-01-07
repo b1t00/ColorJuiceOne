f45e3e78d4ed6d122e10a1a01e31df0d3f0b9c58
b1t00 <48808048+b1t00@users.noreply.github.com>
2020-01-07 20:16:32 +0100

    Sound und Music

---
+++ Assets/SimplePointer/Scripts/SimplePonter/Pointers/CanTriggerSounds.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanTriggerSounds : MonoBehaviour
+{
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if(OVRInput.GetLocalControllerRotation(OVRInput.GetActiveController()).x > 40 || OVRInput.GetLocalControllerRotation(OVRInput.GetActiveController()).z > 40)
+        {
+          
+           //indObjectOfType<AudioManager>().Play("Shake");
+
+            Debug.Log("Jetzt Shaken");
+            GetComponent<AudioSource>().Play();
+        }
+    }
+}
