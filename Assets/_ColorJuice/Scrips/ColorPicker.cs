6107fa04935f46555ca18bd257011643aa8b12fa
AnnCharBaer <48607023+AnnCharBaer@users.noreply.github.com>
2020-01-16 09:27:29 +0100

    farbrad script

--- Assets/_ColorJuice/Scrips/ColorPicker.cs
+++ Assets/_ColorJuice/Scrips/ColorPicker.cs
@@ -14,12 +14,12 @@ public class ColorPicker : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Renderer renderer = hit.transform.getComponent<MeshRenderer>();
+      /*  Renderer renderer = hit.transform.getComponent<MeshRenderer>();
         Texture2D texture = renderer.material.mainTexture as Texture2D;
         Vector2 pixelUV = hit.textureCoord;
         pixelUV.x *= texture.width;
         pixelUV.y *= texture.height;
         Vector2 tiling = renderer.material.mainTextureScale;
-        Color color = imageMap.GetPixel(Mathf.FloorToInt(pixelUV.x * tiling.x), Mathf.FloorToInt(pixelUV.y * tiling.y));
+        Color color = imageMap.GetPixel(Mathf.FloorToInt(pixelUV.x * tiling.x), Mathf.FloorToInt(pixelUV.y * tiling.y));*/
     }
 }
