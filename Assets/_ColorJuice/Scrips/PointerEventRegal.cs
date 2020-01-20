eaacdae5431035c24f98db858654bfe4fe2260c9
NormsChamp <bulletnorm@googlemail.com>
2020-01-20 17:42:51 +0100

    norman hat geburtstag

--- Assets/_ColorJuice/Scrips/PointerEventRegal.cs
+++ Assets/_ColorJuice/Scrips/PointerEventRegal.cs
@@ -6,17 +6,24 @@ using UnityEngine.EventSystems;
 
 //Scriptkomponente, die auf den Objecten für die Farbauswahl liegt
 
-public class PointerEventRegal : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler, IPointerClickHandler
+public class PointerEventRegal: MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler, IPointerClickHandler
 {
     //Farben werden im Inspector festgelegt
     [SerializeField] private Color normalColor; // Die Farbe die zum Sprühen verwendet wird
-    [SerializeField] private Color enterColor;  // Farbe die angezeigt wird, wenn man mit pointer über object geht
-    [SerializeField] private Color downColor; // Farbe die angezeigt wird, wenn man bestätigt (pointer down)
+    //[SerializeField] private Color enterColor;  // Farbe die angezeigt wird, wenn man mit pointer über object geht
+    //[SerializeField] private Color downColor; // Farbe die angezeigt wird, wenn man bestätigt (pointer down)
     [SerializeField] private UnityEvent OnClick = new UnityEvent();
 
     public GameObject can; //sprühdose/object das mit bei der Farbauswahl auswählt
 
     private MeshRenderer meshRenderer = null;
+
+    private Vector3 locPos;
+    bool enter;
+    bool stay;
+
+    private GameObject[] alleCans;
+    
 
 
     private void Awake()
@@ -26,30 +33,56 @@ public class PointerEventRegal : MonoBehaviour, IPointerEnterHandler, IPointerEx
 
         var chooseRenderer = gameObject.GetComponent<Renderer>();
         chooseRenderer.material.SetColor("_Color", normalColor); // Beim Laden wird dem Object die Farbe normalColor zugewiesen
+
+        enter = false;
+        stay = false;
         
+    }
+    private void Start()
+    {
+        alleCans = GameObject.FindGameObjectsWithTag("Can"); //holt sich alle kacheln
+
+    }
+    private void Update()
+    {
+        locPos = gameObject.transform.localPosition;
+
+        if (enter || stay)
+        {
+            locPos.x = -6.32f;
+            gameObject.transform.localPosition = locPos;
+        }
+        else
+        {
+            locPos.x = -6.635f;
+            gameObject.transform.localPosition = locPos;
+        }
+
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        meshRenderer.material.color = enterColor;
+        //eshRenderer.material.color = enterColor;
         print("Enter");
+        enter = true;
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        meshRenderer.material.color = normalColor;
+        //meshRenderer.material.color = normalColor;
         print("Exit");
+        enter = false;
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
-        meshRenderer.material.color = downColor;
+        //meshRenderer.material.color = downColor;
         print("Down");
     }
 
     public void OnPointerUp(PointerEventData eventData)
     {
-        meshRenderer.material.color = enterColor;
+        //meshRenderer.material.color = enterColor;
         print("Up");
     }
 
@@ -59,11 +92,31 @@ public class PointerEventRegal : MonoBehaviour, IPointerEnterHandler, IPointerEx
         print("Click");
         var canRenderer = can.GetComponent<Renderer>();
         canRenderer.material.SetColor("_Color", normalColor);
-        
+
+
+        foreach (GameObject cani in alleCans)
+        {
+            if (cani.name == gameObject.name) //schaut ob kachel aus array diese kachel ist
+            {
+                cani.GetComponent<PointerEventRegal>().SetStay(true);
+            }
+            else
+            {
+                cani.GetComponent<PointerEventRegal>().SetStay(false);
+            }
+        }
+
+
+
     }
 
     public Color GetNormalColor() //public methode um farbe zu übergeben. Wichtig für die Farbauswahl
     {
         return normalColor;
+    }
+
+    public void SetStay(bool stay)
+    {
+        this.stay = stay;
     }
 }
